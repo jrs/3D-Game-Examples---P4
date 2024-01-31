807a922d716247b93f162bfcbb470af55de9a2bc
Justin Scott <hello@jrs.dev>
2024-01-31 12:03:00 -0800

    working on player jumping

--- 3D Game Examples P4/Assets/Scripts/JumpingWithJohn.cs
+++ 3D Game Examples P4/Assets/Scripts/JumpingWithJohn.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class JumpingWithJohn : MonoBehaviour
 {
     public float turnSpeed = 20f;
     public float moveSpeed = 10f;
---
+++ 3D Game Examples P4/Assets/Scripts/RollABall.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollABall : MonoBehaviour
+{
+    public float Speed = 10;
+    private Rigidbody _playerRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput;
+        float verticalInput;
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput);
+        _playerRigidbody.AddForce(movement * Speed);
+    }
+}
