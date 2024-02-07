b8a547eadde01919b494271bb654078ba0ad4e16
Justin Scott <hello@jrs.dev>
2024-02-07 11:47:52 -0800

    Finished checkpoints

--- 3D Game Examples P4/Assets/Scripts/JumpingWithJohn.cs
+++ 3D Game Examples P4/Assets/Scripts/JumpingWithJohn.cs
@@ -8,23 +8,29 @@ public class JumpingWithJohn : MonoBehaviour
     public float moveSpeed = 1f;
     public float JumpForce = 10f;
     public float GravityModifier = 1f;
+    public float outOfBounds = -10f;
     public bool IsOnGround = true;
+    public bool isAtCheckpoint = false;
+    public GameObject checkpointAreaObject;
+    public GameObject finishAreaObject;
     private Vector3 _movement;
     //private Animator _animator;
     private Rigidbody _rigidbody;
     private Quaternion _rotation = Quaternion.identity;
-
-    //private Vector3 _defaultGravity = new Vector3(0f, -9.81f, 0f);
+    private Vector3 _defaultGravity = new Vector3(0f, -9.81f, 0f);
+    private Vector3 _startingPostion;
+    private Vector3 _checkpointPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         //_animator = GetComponent<Animator>();
         _rigidbody = GetComponent<Rigidbody>();
-        //Physics.gravity = _defaultGravity;
+        Physics.gravity = _defaultGravity;
         //Debug.Log(Physics.gravity);
         Physics.gravity *= GravityModifier;
         //Debug.Log(Physics.gravity);
+        _startingPostion = transform.position;
     }
 
     void Update()
@@ -33,6 +39,19 @@ public class JumpingWithJohn : MonoBehaviour
         {
             _rigidbody.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
             IsOnGround = false;
+        }
+
+        if(transform.position.y < outOfBounds)
+        {
+            if(isAtCheckpoint)
+            {
+                transform.position = _checkpointPosition;
+            }
+            else
+            {
+                transform.position = _startingPostion;
+            }
+            
         }
     }
 
@@ -69,6 +88,23 @@ public class JumpingWithJohn : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             IsOnGround = true;
+        }
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == checkpointAreaObject)
+        {
+            isAtCheckpoint = true;
+            //Debug.Log(_startingPostion);
+            _checkpointPosition = checkpointAreaObject.transform.position;
+            //Debug.Log(_startingPostion);
+        }
+
+        if(other.gameObject == finishAreaObject)
+        {
+            isAtCheckpoint = false;
+            transform.position = _startingPostion;
         }
     }
 }
